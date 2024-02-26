c1378b57dd8c8d792301b854461cb3684e2dcc8a
Максим Соловьев <mvs@kassa24.kz>
2024-02-26 15:03:53 +0600

    Update BlockExplorer class

--- src/classes/BlockExplorer.ts
+++ src/classes/BlockExplorer.ts
@@ -1,8 +1,11 @@
+import {
+  BlockExplorer,
+  GetAccountBalanceOptions,
+  GetAccountTokenBalanceOptions,
+  GetBlockNumberOptions
+} from '../interfaces/BlockExplorer';
 import { Chain } from '../types/chains';
-
-export interface BlockExplorer {
-  getBlockId: () => Promise<number>;
-}
+import { ChainType } from '../types/routescan';
 
 export interface BlockExplorerOptions {
   /**
@@ -12,9 +15,43 @@ export interface BlockExplorerOptions {
   /**
    * @description API key to work with blockchain explorer
    */
-  apiKey: string;
+  apiKey?: string;
 }
 
-class BlockExplorerRoutescan {
-  constructor(options: BlockExplorerOptions) {}
+abstract class BlockExplorerCommon implements BlockExplorer {
+  protected apiKey: string = '';
+  protected chain: Chain = Chain.NotSpecified;
+
+  constructor(options: BlockExplorerOptions) {
+    const { apiKey, chain } = options;
+    if (apiKey) {
+      this.apiKey = apiKey;
+    }
+    if (chain) {
+      this.chain = chain;
+    }
+  }
+
+  abstract getBlockNumber: (options: GetBlockNumberOptions) => Promise<number>;
+  abstract getAccountBalance: (options: GetAccountBalanceOptions) => Promise<bigint>;
+  abstract getAccountTokenBalance: (options: GetAccountTokenBalanceOptions) => Promise<bigint>;
+}
+
+export class BlockExplorerRoutescan extends BlockExplorerCommon {
+  constructor(options: BlockExplorerOptions) {
+    super(options);
+  }
+
+  public getBlockNumber(options: GetBlockNumberOptions) {}
+
+  public getAccountBalance(options: GetAccountBalanceOptions) {}
+
+  public getAccountTokenBalance(options: GetAccountTokenBalanceOptions) {}
+
+  public getBlockExplorerUrl(): string {
+    const { chain, is_main_net } = this;
+    const { url } = this.config;
+    const chainType = is_main_net ? ChainType.MainNet : ChainType.TestNet;
+    return `${url}/${chainType}/evm/${chain}/etherscan/api`;
+  }
 }
