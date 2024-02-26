8adc0c21fc67839830af249810b0ff28f6adcda7
Максим Соловьев <mvs@kassa24.kz>
2024-02-26 15:47:05 +0600

    Update BlockExplorer class

--- src/classes/BlockExplorer.ts
+++ src/classes/BlockExplorer.ts
@@ -1,11 +1,14 @@
 import {
   BlockExplorer,
+  BlockExplorerType,
   GetAccountBalanceOptions,
   GetAccountTokenBalanceOptions,
   GetBlockNumberOptions
 } from '../interfaces/BlockExplorer';
 import { Chain } from '../types/chains';
 import { ChainType } from '../types/routescan';
+
+import { chains } from '../data/chains';
 
 export interface BlockExplorerOptions {
   /**
@@ -35,6 +38,28 @@ abstract class BlockExplorerCommon implements BlockExplorer {
   abstract getBlockNumber: (options: GetBlockNumberOptions) => Promise<number>;
   abstract getAccountBalance: (options: GetAccountBalanceOptions) => Promise<bigint>;
   abstract getAccountTokenBalance: (options: GetAccountTokenBalanceOptions) => Promise<bigint>;
+
+  public static build(options: BlockExplorerOptions): BlockExplorer {
+    const { chain } = options;
+    if (!chain) {
+      throw new Error('Please specify the chain id');
+    }
+
+    const chainOptions = chains.find(({ id }) => id === chain);
+    if (!chainOptions) {
+      throw new Error(`Chain with id of ${chain} is not supported`);
+    }
+
+    const { blockExplorerType } = chainOptions;
+    switch (blockExplorerType) {
+      case BlockExplorerType.Ethereum:
+        return new BlockExplorerEthereum(options);
+      case BlockExplorerType.Routescan:
+        return new BlockExplorerRoutescan(options);
+      case BlockExplorerType.Chainlens:
+        return new BlockExplorerChainlens(options);
+    }
+  }
 }
 
 export class BlockExplorerRoutescan extends BlockExplorerCommon {
@@ -42,16 +67,27 @@ export class BlockExplorerRoutescan extends BlockExplorerCommon {
     super(options);
   }
 
-  public getBlockNumber(options: GetBlockNumberOptions) {}
+  public getBlockNumber = async (options: GetBlockNumberOptions) => {
+    return 1;
+  };
 
-  public getAccountBalance(options: GetAccountBalanceOptions) {}
+  public getAccountBalance = async (options: GetAccountBalanceOptions) => {
+    return 1n;
+  };
 
-  public getAccountTokenBalance(options: GetAccountTokenBalanceOptions) {}
+  public getAccountTokenBalance = async (options: GetAccountTokenBalanceOptions) => {
+    return 1n;
+  };
 
   public getBlockExplorerUrl(): string {
-    const { chain, is_main_net } = this;
+    return '';
+    /* const { chain, is_main_net } = this;
     const { url } = this.config;
     const chainType = is_main_net ? ChainType.MainNet : ChainType.TestNet;
-    return `${url}/${chainType}/evm/${chain}/etherscan/api`;
+    return `${url}/${chainType}/evm/${chain}/etherscan/api`; */
   }
 }
+
+export class BlockExplorerEthereum extends BlockExplorerRoutescan {}
+
+export class BlockExplorerChainlens extends BlockExplorerRoutescan {}
