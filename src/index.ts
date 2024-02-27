6cfe101a35e6b7344a71066e7b9c44d4691a17b2
Максим Соловьев <mvs@kassa24.kz>
2024-02-27 20:02:02 +0600

    Add some enums export

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,16 @@ import {
 } from './classes/BlockExplorer';
 import { Chain } from './types/chains';
 import { BlockExplorer } from './interfaces/BlockExplorer';
+import { BlockExplorerTag, BlockExplorerAction, BlockExplorerClosest } from './types/block-explorer';
 
-export { BlockExplorer, BlockExplorerCommon, BlockExplorerEthereum, BlockExplorerRoutescan, BlockExplorerOptions, Chain };
+export {
+  BlockExplorerTag,
+  BlockExplorerAction,
+  BlockExplorerClosest,
+  BlockExplorer,
+  BlockExplorerCommon,
+  BlockExplorerEthereum,
+  BlockExplorerRoutescan,
+  BlockExplorerOptions,
+  Chain
+};
